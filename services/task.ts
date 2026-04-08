05445ab9f8ce07497cb62e9e2aa1c53bb7e4c89b
Kiran Capoor <kiran.capoor94@gmail.com>
2026-04-08 11:53:53 +0530

    full reset

--- services/task.ts
+++
@@ -1,6 +0,0 @@
-import { getTaskContext } from "../data/repositories/task.js";
-import type { TaskContext } from "../shared/types.js";
-
-export async function lookupTask(taskId: number): Promise<TaskContext | null> {
-  return getTaskContext(taskId);
-}
